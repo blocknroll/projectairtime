e1893547aa8c0aec0f5d6c0f48ee84267723ba7d
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-11-18 20:29:55 -0600

    Update form.js and styling in formStyles.module.scss

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Container from "../components/container"
 import formStyles from "../styles/formStyles.module.scss"
 import navStyles from "../styles/navStyles.module.scss"
 
@@ -7,124 +8,124 @@ export default function Form() {
     <section className={formStyles.section}>
       <div className={navStyles.anchor} id="contact"></div>
 
-      <div className={formStyles.container}>
-        <h2>Contact Us!</h2>
+      <Container>
+        <div className={formStyles.grid}>
+          <h2>Contact Us!</h2>
 
-        <form
-          name="projectairtime-contact-form"
-          method="POST"
-          netlify
-          data-netlify="true"
-          netlify-honeypot="address"
-          action="/success"
-        >
-          <input
-            name="form-name"
-            type="hidden"
-            value="projectairtime-contact-form"
-          />
-
-          <label for="name">
-            Name
+          <form
+            name="projectairtime-contact-form"
+            method="POST"
+            netlify
+            data-netlify="true"
+            netlify-honeypot="address"
+            action="/success"
+          >
             <input
-              required
-              id="name"
-              name="name"
-              autocomplete="name"
-              autocorrect="off"
-              type="text"
+              name="form-name"
+              type="hidden"
+              value="projectairtime-contact-form"
             />
-          </label>
 
-          <label for="phone">
-            Phone
-            <input
-              required
-              id="phone"
-              name="phone"
-              autocomplete="phone"
-              autocorrect="off"
-              type="text"
-            />
-          </label>
-
-          <label for="email">
-            Email
-            <input
-              required
-              id="email"
-              name="email"
-              autocomplete="email"
-              autocorrect="off"
-              type="text"
-            />
-          </label>
-
-          <fieldset>
-            <legend>I would like to...</legend>
-
-            <label>
-              go flying!
+            <label for="name">
+              Name
               <input
-                type="radio"
-                name="interest"
-                id="copilot"
-                value="copilot"
-              />
-              <span></span>
-            </label>
-
-            <label>
-              be a volunteer!
-              <input
-                type="radio"
-                name="interest"
-                id="volunteer"
-                value="volunteer"
-              />
-              <span></span>
-            </label>
-
-            <label>
-              be a sponsor!
-              <input
-                type="radio"
-                name="interest"
-                id="sponsor"
-                value="sponsor"
-              />
-              <span></span>
-            </label>
-          </fieldset>
-
-          <label for="message">
-            Please tell us about yourself...
-            <textarea
-              id="message"
-              name="message"
-              type="text"
-              autocorrect="off"
-              dir="auto"
-            ></textarea>
-          </label>
-
-          <p style={{ display: "none" }}>
-            <label>
-              Please don't enter anything here if you are a person:
-              <input
-                id="address"
-                name="address"
+                required
+                id="name"
+                name="name"
+                autocomplete="name"
+                autocorrect="off"
                 type="text"
-                autocomplete="off"
               />
             </label>
-          </p>
 
-          <p>
+            <label for="phone">
+              Phone
+              <input
+                required
+                id="phone"
+                name="phone"
+                autocomplete="phone"
+                autocorrect="off"
+                type="text"
+              />
+            </label>
+
+            <label for="email">
+              Email
+              <input
+                required
+                id="email"
+                name="email"
+                autocomplete="email"
+                autocorrect="off"
+                type="text"
+              />
+            </label>
+
+            <fieldset>
+              <legend>I would like to...</legend>
+
+              <label>
+                go flying!
+                <input
+                  type="radio"
+                  name="interest"
+                  id="copilot"
+                  value="copilot"
+                />
+                <span></span>
+              </label>
+
+              <label>
+                be a volunteer!
+                <input
+                  type="radio"
+                  name="interest"
+                  id="volunteer"
+                  value="volunteer"
+                />
+                <span></span>
+              </label>
+
+              <label>
+                be a sponsor!
+                <input
+                  type="radio"
+                  name="interest"
+                  id="sponsor"
+                  value="sponsor"
+                />
+                <span></span>
+              </label>
+            </fieldset>
+
+            <label for="message">
+              Please tell us about yourself...
+              <textarea
+                id="message"
+                name="message"
+                type="text"
+                autocorrect="off"
+                dir="auto"
+              ></textarea>
+            </label>
+
+            <p style={{ display: "none" }}>
+              <label>
+                Please don't enter anything here if you are a person:
+                <input
+                  id="address"
+                  name="address"
+                  type="text"
+                  autocomplete="off"
+                />
+              </label>
+            </p>
+
             <input name="submit" type="submit" value="Send It!" />
-          </p>
-        </form>
-      </div>
+          </form>
+        </div>
+      </Container>
     </section>
   )
 }
