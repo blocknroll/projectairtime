de55cf6a628d72d0237147555f960e5eca21ae02
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-11-19 10:39:57 -0600

    Update intro.js and styling in introStyles.module.scss

--- src/components/intro.js
+++ src/components/intro.js
@@ -10,34 +10,36 @@ export default function Intro() {
       <div className={navStyles.anchor} id="intro"></div>
 
       <Container>
-        <h2>What is Project Airtime?</h2>
-        <p>
-          Project Airtime is a Nevada 501(c)(3) non-profit based in Draper,
-          Utah. We take EVERYONE flying. When we say EVERYONE, we mean it. No
-          exclusions. From special needs individuals to those with brain and
-          spinal cord injuries. Individuals with illness, as well as the elderly
-          and veterans. Our co-pilots have one thing in common, they want to
-          fly!
-        </p>
-        <h2>How Do I Go Flying?</h2>
-        <p>
-          Submit the co-pilot <Link to="/#contact">form</Link> and Chris will
-          contact you to work out your Project Airtime flight details. You'll go
-          over travel and lodging logistics, as well as any flying specifics. If
-          you have a question about flying, he has the answer. Flying normally
-          takes place at Point of the Mountain, Utah, but can be arranged in
-          other locations as well.
-        </p>
-        <h2>How Can I Help Out?</h2>
-        <p>
-          We're always looking for people or organizations that are willing to
-          volunteer time/skill or become a sponsor. If you have a skill that you
-          think would be of service to the team, please fill out our{" "}
-          <Link to="/#contact">form</Link> below, or shoot us an email and we'll
-          be in touch. We're currently looking for people with expertise in
-          partnerships, promotions and fundraising, though all support is
-          welcome.
-        </p>
+        <div className={introStyles.grid}>
+          <h2>What is Project Airtime?</h2>
+          <p>
+            Project Airtime is a Nevada 501(c)(3) non-profit based in Draper,
+            Utah. We take EVERYONE flying. When we say EVERYONE, we mean it. No
+            exclusions. From special needs individuals to those with brain and
+            spinal cord injuries. Individuals with illness, as well as the
+            elderly and veterans. Our co-pilots have one thing in common, they
+            want to fly!
+          </p>
+          <h2>How Do I Go Flying?</h2>
+          <p>
+            Submit the co-pilot <Link to="/#contact">form</Link> and Chris will
+            contact you to work out your Project Airtime flight details. You'll
+            go over travel and lodging logistics, as well as any flying
+            specifics. If you have a question about flying, he has the answer.
+            Flying normally takes place at Point of the Mountain, Utah, but can
+            be arranged in other locations as well.
+          </p>
+          <h2>How Can I Help Out?</h2>
+          <p>
+            We're always looking for people or organizations that are willing to
+            volunteer time/skill or become a sponsor. If you have a skill that
+            you think would be of service to the team, please fill out our{" "}
+            <Link to="/#contact">form</Link> below, or shoot us an email and
+            we'll be in touch. We're currently looking for people with expertise
+            in partnerships, promotions and fundraising, though all support is
+            welcome.
+          </p>
+        </div>
       </Container>
     </section>
   )
