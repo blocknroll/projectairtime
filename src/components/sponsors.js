4d28730bcb022b980c398be2ee4375b3c9f3f6b5
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-11-18 19:08:55 -0600

    Update sponsors.js and styling in sponsorStyles.module.scss

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Container from "../components/container"
 import sponsorStyles from "../styles/sponsorStyles.module.scss"
 import navStyles from "../styles/navStyles.module.scss"
 
@@ -7,140 +8,63 @@ export default function Sponsors() {
     <section className={sponsorStyles.section}>
       <div className={navStyles.anchor} id="sponsors"></div>
 
-      <div className={sponsorStyles.container}>
-        <h2>Sponsors + Supporters</h2>
-
-        <p>
-          Project Airtime has many great supporters. It's nearly impossible to
-          thank every single contributor but you know who you are. From those
-          that "like" and share our videos, to the foundations that fund our
-          efforts, every supporter counts. You are Project Airtime! This is your
-          organization and we will do everything we can to make you proud of it.
-        </p>
-
+      <Container>
         <div className={sponsorStyles.grid}>
-          <div className={sponsorStyles.sponsor}>
+          <h2>Sponsors + Supporters</h2>
+
+          <p>
+            Project Airtime has many great supporters. It's nearly impossible to
+            thank every single contributor but you know who you are. From those
+            that "like" and share our videos, to the foundations that fund our
+            efforts, every supporter counts. You are Project Airtime! This is
+            your organization and we will do everything we can to make you proud
+            of it.
+          </p>
+
+          <div className={sponsorStyles.subgrid}>
             <p>The Gubelman Foundation</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Taka Investments</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Jonathan Foster</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Shad Coulson</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Curtis Williams</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Crystal, Madison and Cameron Lee</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Matt Hotchkiss</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Julie Williams</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Lori Dirks</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Luanne Plot Hording</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>John Schnebeck</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Luke Seile</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Sandefer Family</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Carl Appelin</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>The Fabric of America Fund</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Mark Burnett</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Jeff Howard</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Runolfson Family</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>John Lau</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Mark and Bo Rich</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Harrison Ruffin</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Jason and Latisha Joseph</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Tom Keefer</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Blake and Olivia Pelton</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Joe Stone</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Tony Lang</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Johnathan Foster</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>The Community Foundation of Utah</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Foundation for Free Flight</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Marc Angelo</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Lori Fitzgerald</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Rodrigo Jordan</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Steve Thorne</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Bill Liscomb</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Daniel Kotter</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Grant and Shawna Korgan</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Randy Leggett</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Tom and Patricia Hampton</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>Loren Cox</p>
-          </div>
-          <div className={sponsorStyles.sponsor}>
             <p>BecomeCo</p>
           </div>
         </div>
-      </div>
+      </Container>
     </section>
   )
 }
