e89547f1bbcb3d9703ef6aa6cfe08d5b348b316a
Fred Block <5204778+blocknroll@users.noreply.github.com>
2021-03-02 12:12:26 -0600

    Update video.js with new test videos and pages

---
+++ src/pages/greenhouse.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Container from "../components/container"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import videoStyles from "../styles/videoStyles.module.scss"
+
+export default function Success({ data }) {
+  return (
+    <Layout>
+      <section className={videoStyles.section}>
+        <Container>
+          <div className={videoStyles.grid}>
+            <iframe
+              src="https://www.youtube-nocookie.com/embed/9jbLZxwWudk"
+              title="greenhouse"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              frameBorder="0"
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+              allowFullScreen
+              width="560"
+              height="315"
+            />
+            <h3 className={videoStyles.caption}>greenhouse</h3>
+
+            <div className={videoStyles.container}>
+              <p>
+                back to the <Link to="/#video">video section...</Link>
+              </p>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </Layout>
+  )
+}
