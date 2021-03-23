86c1241977e5bd7484ee8776a688d162916d771b
Fred Block <5204778+blocknroll@users.noreply.github.com>
2021-03-23 11:39:04 -0500

    Add myoverdrive.js. Update about.js and youtube.js. Update about and video styling

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -2,28 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 import videoStyles from "../styles/videoStyles.module.scss"
 
-const Youtube = ({ videoSrcURL, videoTitle, videoCaption }) => (
-    <div>
-      <div className={videoStyles.youtubeIframeContainer}>
-        <iframe
-          src={videoSrcURL}
-          title={videoTitle}
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          frameBorder="0"
-          webkitallowfullscreen="true"
-          mozallowfullscreen="true"
-          allowFullScreen
-          width="560"
-          height="315"
-        />
-      </div>
-
-      <h3>{videoCaption}</h3>
-
-      <p>
-        back to the <Link to="/#videos">video</Link> section.
-      </p>
+const Youtube = ({ videoSrcURL, videoTitle, videoCaption, returnLink }) => (
+  <div>
+    <div className={videoStyles.youtubeIframeContainer}>
+      <iframe
+        src={videoSrcURL}
+        title={videoTitle}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowFullScreen
+        width="560"
+        height="315"
+      />
     </div>
+
+    <h3>{videoCaption}</h3>
+
+    <p>
+      back to the <Link to={`/#${returnLink}`}>{returnLink}</Link> section.
+    </p>
+  </div>
 )
 
 export default Youtube
