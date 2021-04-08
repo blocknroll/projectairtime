68f2ad5d38dd0534604c3ac5a296a48bcde613ab
Fred Block <5204778+blocknroll@users.noreply.github.com>
2021-04-08 16:37:58 -0500

    Update success.js with StaticImage for the hero image

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,7 +1,6 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { Link } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import successStyles from "../styles/successStyles.module.scss"
@@ -11,12 +10,14 @@ export default function Success({ data }) {
     <Layout>
       <SEO title="Success" />
       <section className={successStyles.section}>
-        <GatsbyImage
-          image={data.ChrisThumbsUp.childImageSharp.gatsbyImageData}
-          className={successStyles.imgResponsive}
-          alt="Chris Santacroce and his co-pilot paragliding at Point of the Mountain in Draper, Utah." />
+        <StaticImage
+          src="../images/Chris-thumbsup.jpg"
+          alt="Chris Santacroce and his co-pilot paragliding at Point of the Mountain in Draper, Utah."
+        />
+
         <div className={successStyles.container}>
           <h1>"YEAH BUDDY!!!"</h1>
+
           <h3>
             Thank you so much{" "}
             <span role="img" aria-label="folded hands: medium-light skin tone">
@@ -24,6 +25,7 @@ export default function Success({ data }) {
             </span>
             <br></br>We'll be in contact soon!
           </h3>
+
           <p>
             In the meantime, let's head back to the{" "}
             <Link to="/">homepage.</Link>
@@ -31,15 +33,5 @@ export default function Success({ data }) {
         </div>
       </section>
     </Layout>
-  );
+  )
 }
-
-// paths relative to "./src/images/" defined in gatsby-config.js > gatsby-source-filesystem
-export const query = graphql`{
-  ChrisThumbsUp: file(relativePath: {eq: "Chris-thumbsup.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-    }
-  }
-}
-`
